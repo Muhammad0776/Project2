295724293bfa3829f04e91c6baef1940cd1f284e
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-07-10 17:55:54 +0500

    Card.js App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from "./Card"
+
+const data = [
+  {
+    img: "https://img3.goodfon.ru/original/1920x1408/f/e8/beautiful-landscape-les.jpg",
+    title: "БЕЛАЯ ВОРОНА",
+    subtitle: "Лига сервер Россия-1- Страница 20 - Big Farm"
+  },
+  {
+    img: "https://wallpaperscave.ru/images/original/18/01-16/earth-lake-10484.jpg",
+    title: "Озеро скачать",
+    subtitle: "Озеро скачать заставку на рабочий стол"
+  },
+  {
+    img: "https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701356348.jpg",
+    title: "These 3D",
+    subtitle: "These 3D images of nature are the best of"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mt-5">
+
+          {
+            data.map((value, index) => {
+
+              return <div className="col-md-4">
+                <Card
+                  img={value.img}
+                  title={value.title}
+                  subtitle={value.subtitle}
+                />
+              </div>
+
+            })
+          }
+
+        </div>
+      </div>
     </div>
   );
 }
